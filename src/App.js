ce496f134a9a5f5d9e11cf54efedca49916d8d21
Enron Hubbard <tygerhardt@gmail.com>
2020-10-10 10:00:04 -0700

    Add switch.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
-  Route,
+  Route, Switch
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './contents/Home';
@@ -13,28 +13,30 @@ import Contact from './contents/Contact';
 
 function App() {
   return (
-<Router>
-<div className="App">
-<Navbar />
-{/* Route for Home.js contents */}
-{/* Route for About.js contents */}
-<Route path="/about">
-<About />
-</Route>
-{/* Route for Portfolio.js contents */}
-<Route path="/portfolio">
-<Portfolio />
-</Route>
-{/* Route for Contact.js contents */}
-<Route path="/contact">
-<Contact />
-</Route>
-<Route path="/">
-<Home />
-</Route>
-</div>
-</Router>
-    )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch >
+          {/* Route for Home.js contents */}
+          {/* Route for About.js contents */}
+          <Route path="/about">
+            <About />
+          </Route>
+          {/* Route for Portfolio.js contents */}
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          {/* Route for Contact.js contents */}
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 export default App;
 
